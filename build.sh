141b3834f6e5547fc8ca0317601cf7decc29f8bf
Neil Prescott <github@neilprescott.com>
2021-10-14 10:15:27 +0000

    NP: added build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+handleError() {
+	msg=$1
+	rc=$2
+	echo "ERROR detected - message: '${msg}' code i{$ra}c"
+	exit $rc
+}
+npm build ~/exercises/REST-API-starter/ || handleError "NPM Build failed" $?
+
+
+#-Build Docker image of your application.
+docker build -t restapp-neil-iamge:v1 ~/exercises/REST-API-starter/ || handleError "Docker Build failed" $?
+
+#-Modify your application.
+echo 'Adding in something new' > ~/exercises/REST-API-starter/newfile.txt || handleError "echo/file creat failede" $?
+
+#-Generate new Docker image of an application.
+docker build -t restapp-neil-image:v2 ~/exercises/REST-API-starter/ || handleError "Docker Build failed" $?
